65a04296f87c17cc66584657af5137bec1220e49
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-03 10:09:10 -0500

    refactor: remove unused import in UserQueryService

--- authentication/src/main/java/com/example/authentication/management/domain/services/UserQueryService.java
+++ authentication/src/main/java/com/example/authentication/management/domain/services/UserQueryService.java
@@ -3,8 +3,6 @@ package com.example.authentication.management.domain.services;
 import com.example.authentication.management.domain.model.aggregates.User;
 import com.example.authentication.management.domain.model.queries.GetUserByIdQuery;
 
-import java.util.Optional;
-
 public interface UserQueryService {
 
   User getUserById(GetUserByIdQuery query);
